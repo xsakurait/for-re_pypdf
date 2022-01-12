9db2c9bce32aec72b2565c9cbdb56841187210f3
xsakurait <60243830+xsakurait@users.noreply.github.com>
2022-01-12 19:33:41 +0900

    Add files via upload

---
+++ pdf連結.py
@@ -0,0 +1,16 @@
+# 正規表現使う場合
+# import PyPDF2
+# import re,os
+# merger = PyPDF2.PdfFileMerger()
+# path='C:\Users\Apple\Downloads'
+# text=os.list(path)
+# pattern='PL81-\d\d'
+# results=re.findall(pattern,text)
+#for result in results:
+#     merger.append('')
+# with open('path'+'\pypdf','wb') as f:
+    #  merge.write(f)
+
+# for文使う場合（こっちのほうが簡単だがファイル数把握してないと使えない）
+
+
